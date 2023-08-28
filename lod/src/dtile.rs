6c0ea211d2c18924396f85165fc5e52456240bff
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2023-08-28 20:32:36 +0200

    refactor(dtile): refactor dtile.bin (#3)

--- lod/src/dtile.rs
+++ lod/src/dtile.rs
@@ -52,41 +52,22 @@ impl Dtile {
 
     pub fn table(&self, tile_data: [u16; 8]) -> Result<TileTable, Box<dyn Error>> {
         let mut names_table: Vec<String> = Vec::with_capacity(256);
-        for i in 0..=255 {
-            // This is an hardcoded decoding of the index since I can't yet make full sense of the dtile.bin
-            let name = index_to_tile_name_hack(&tile_data, i as u8);
-            if name != "pending" {
-                //dbg!("idx:{}, name:{} (hack)", i, name);
-                names_table.push(name);
-                continue;
-            }
-
-            let index = if i >= 198 {
-                // roads
-                i as u16 - 198 + tile_data[7]
-            } else if i < 90 {
-                i as u16 //+ tile_data[1]
-                         // } else if (126..198).contains(&i) {
-                         //     i - 126 + tile_data[5]
+        println!("{tile_data:?}");
+        for i in 0_u16..=255_u16 {
+            let index = if (90..125).contains(&i) {
+                i - 90 + tile_data[1] // primary
+            } else if (126..161).contains(&i) {
+                i
+                //i - 126 + tile_data[3] // water
+            } else if (162..197).contains(&i) {
+                i - 162 + tile_data[5] // secondary
+            } else if i >= 198 {
+                i - 198 + tile_data[7] // roads
             } else {
-                // borders
-                let n = 2 * (i as u16 - 90) / 36;
-                tile_data[n as usize]
+                i // dirt
             };
 
-            let mut dtile = self.read(index as usize)?;
-
-            if dtile.c == 0x300 {
-                let group = dtile.a;
-                for j in 0..4 {
-                    if tile_data[j * 2] == group {
-                        let index2 = tile_data[j * 2 + 1];
-                        dtile = self.read(index2 as usize)?;
-                        break;
-                    }
-                }
-            }
-            //dbg!("idx:{}, name:{} (dtile)", i, dtile.name);
+            let dtile: DtileData = self.read(index as usize)?;
             names_table.push(dtile.name);
         }
 
@@ -179,100 +160,7 @@ impl TileTable {
 
     pub fn atlas_image(&self, lod_manager: &LodManager) -> Result<DynamicImage, Box<dyn Error>> {
         let ts: Vec<&str> = self.names_set.iter().map(|s| s.as_str()).collect();
-        get_atlas(&lod_manager, ts.as_slice(), self.size.0 as usize)
-    }
-}
-
-#[deprecated]
-fn index_to_tile_name_hack(tile_data: &[u16; 8], index: u8) -> String {
-    if (1..=0x34).contains(&index) {
-        return "dirttyl".into();
-    }
-
-    if (0x5a..=0x65).contains(&index) {
-        return get_tile_group_hack(tile_data, 0).0;
-    }
-    if let Some(name) = get_composed_tile_group(tile_data, 0, index, 0x66) {
-        return name.1;
-    }
-
-    if (0xa2..=0xad).contains(&index) {
-        return get_tile_group_hack(tile_data, 4).0;
-    }
-    if let Some(name) = get_composed_tile_group(tile_data, 4, index, 0xae) {
-        return name.1;
-    }
-
-    if (0x7e..=0x89).contains(&index) {
-        return get_tile_group_hack(tile_data, 6).0;
-    }
-    if let Some(name) = get_composed_tile_group(tile_data, 6, index, 0x8a) {
-        return name.1;
-    }
-
-    "pending".into()
-}
-
-#[deprecated]
-fn get_tile_group_hack(tile_data: &[u16; 8], group: usize) -> (String, String) {
-    let id1 = tile_data[group];
-    let id2 = tile_data[group + 1];
-
-    if id1 == 0 && id2 == 90 {
-        return ("grastyl".into(), "grdrt".into());
-    }
-    if id1 == 1 && id2 == 342 {
-        return ("snotyl".into(), "snodr".into());
-    }
-    if id1 == 2 && id2 == 234 {
-        return ("sandtyl".into(), "sndrt".into());
-    }
-    if id1 == 3 && id2 == 198 {
-        return ("voltyl".into(), "voldrt".into());
-    }
-    if id1 == 6 && id2 == 162 {
-        return ("crktyl".into(), "crkdrt".into());
-    }
-    if id1 == 7 && id2 == 270 {
-        return ("swmtyl".into(), "swmdr".into());
-    }
-    if id1 == 8 && id2 == 306 {
-        return ("troptyl".into(), "trop".into());
-    }
-    if id1 == 22 && id2 == 774 {
-        return ("wtrtyl".into(), "wtrdr".into());
-    }
-    ("pending".into(), "pending".into())
-}
-
-#[deprecated]
-fn get_composed_tile_group(
-    tile_data: &[u16; 8],
-    group: usize,
-    index: u8,
-    code: u8,
-) -> Option<(String, String)> {
-    let suffix = get_tile_type(index, code)?;
-    let mut group = get_tile_group_hack(tile_data, group);
-    group.1 = format!("{}{}", group.1, suffix);
-    Some(group)
-}
-
-#[deprecated]
-fn get_tile_type(index: u8, code: u8) -> Option<&'static str> {
-    let group_type = [
-        "ne", "se", "nw", "sw", "e", "w", "n", "s", "xne", "xse", "xnw", "xsw",
-    ];
-
-    if index < code {
-        return None;
-    }
-
-    let offset = index - code;
-    if offset < 12 {
-        Some(group_type[offset as usize])
-    } else {
-        None
+        get_atlas(lod_manager, ts.as_slice(), self.size.0 as usize)
     }
 }
 
@@ -309,18 +197,7 @@ mod tests {
         tile_table
             .atlas_image(&lod_manager)
             .unwrap()
-            .save("map_viewer/assets/terrain_atlas.png")
+            .save("terrain_atlas.png")
             .unwrap();
-        dbg!("{:?}", tile_table.size());
-        dbg!(
-            "{:?} -> {:?}",
-            tile_table.name(22),
-            tile_table.coordinate(22)
-        );
-        dbg!(
-            "{:?} -> {:?}",
-            tile_table.name(90),
-            tile_table.coordinate(90)
-        );
     }
 }
