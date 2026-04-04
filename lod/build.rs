b771818fce9644adc6766715310b112f663763db
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2026-04-05 01:52:56 +0200

    fix(lod): add rerun-if-changed=build.rs and version comment

--- lod/build.rs
+++ lod/build.rs
@@ -1,4 +1,7 @@
+// Vendored from greg-kennedy/libsmacker v1.2.0
+// https://github.com/greg-kennedy/libsmacker
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     cc::Build::new()
         .file("vendor/libsmacker/smacker.c")
         .include("vendor/libsmacker")
